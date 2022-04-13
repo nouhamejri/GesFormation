c78992521b6dbfa1fcc28974d2ddc4cc7335dc60
AymenJeljli <aymenjeljli9@gmail.com>
2022-04-13 16:46:30 +0100

    first

--- src/main/java/com/esprit/examen/entities/Cours.java
+++ src/main/java/com/esprit/examen/entities/Cours.java
@@ -10,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
+//AJ
 @Entity
 public class Cours implements Serializable {
 	private static final long serialVersionUID = 1L;
